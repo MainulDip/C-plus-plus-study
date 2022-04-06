4691e21ca7d745a390bb2ecebbb23dde5ddcfd47
mainuldip <mainuldip@gmail.com>
2022-04-06 05:41:18 -0400

    docs getting pointers arithmetic

--- CrashCourseCpp/ConsoleApplication/ConsoleApplication.cpp
+++ CrashCourseCpp/ConsoleApplication/ConsoleApplication.cpp
@@ -18,33 +18,25 @@ int main()
 	cout << "size(primeNums) : " << lenArr << " && sizeof(primeNums) :" << sizeof(primeNums) << endl;
 	// size(primeNums) : 7 && sizeof(primeNums) :28
 
-	//int lenArrP = *(&primeNums + 1) - primeNums;
-	cout << "*(&primeNums + 1) : "<< *(&primeNums + 1) << " && *(&primeNums + 1) - primeNums : " << *(&primeNums + 1) - primeNums << " && *(&primeNums - 1) - primeNums : " << *(&primeNums - 1) - primeNums << endl;
-	// *(&primeNums + 1) : 000000966CB6F5A4 && *(&primeNums + 1) - primeNums : 7 && *(&primeNums - 1) - primeNums : -7
-
-	cout << "primeNums[1] : " << primeNums[1] << " && &primeNums[1] : " << &primeNums[1] << " && *(&primeNums[1]) : " << *(&primeNums[1]) << endl;
-	// primeNums[1] : 2 && &primeNums[1] : 000000A98B8FF9DC && *(&primeNums[1]) : 2
-
-	cout << "&primeNums : " << &primeNums << " && *(&primeNums) : " << *(&primeNums) << " && *(&primeNums[1] : " << *(&primeNums[1]) << " && *primeNums : " << *primeNums << " && *primeNums + 1 : "<< *primeNums + 1 << endl;
-	// &primeNums : 000000E6324FF4C8 && *(&primeNums) : 000000E6324FF4C8 && *(&primeNums[1] : 2 && *primeNums : 1 && *primeNums + 1 : 2
-
-	cout << " &primeNums + 1 : " << &primeNums + 1 << " && primeNums + 1 : " << primeNums + 1 << " && *primeNums + 1 : " << *primeNums + 1 << " && *(&primeNums + 1) : " << *(&primeNums +1) << " && *(&primeNums) + 1 : " << *(&primeNums) + 1 << endl;
-	// &primeNums + 1 : 00000098DACFFA94 && primeNums + 1 : 00000098DACFFA7C && *primeNums + 1 : 2 && *(&primeNums + 1) : 00000098DACFFA94 && *(&primeNums) + 1 : 00000098DACFFA7C
-	
 	// with reference, have to mention array index, and dereference using *
 	cout << "&primeNums[1] : " << &primeNums[1] << " && *(&primeNums[1]) : " << *(&primeNums[1]) << endl;
 	// &primeNums[1] : 0000004EB936F5AC && *(&primeNums[1]) : 2
 
 
-	// forcing return using unary plus operator
+	// forcing int size return using unary plus operator, otherwise it will return byte size of the array
 	cout << sizeof(+primeNums) - 1 << endl;
 	// 7
 
 	// Now Lets find how *(&primeNums + 1) - primeNums : 7 works
 	cout << "\n\nNow Lets find how *(&primeNums + 1) - primeNums : 7 works\n\n" << endl;
 
-	// this is reducing (minusing) address from address, may be it is using unary minus operator like unary plus;
-	cout << "primeNums : " << primeNums << " && *(&primeNums + 1): " << *(&primeNums + 1) << endl;
+	// *(&primeNums + 1) returns a pointer and primeNums is the pointer of the first element.
+	cout << "\n\n &primeNums : " << &primeNums << " and &primeNums + 1 : " << &primeNums + 1 << " and primeNums : " << primeNums << endl;
+	cout << "\n\n *(&primeNums + 1) - primeNums : " << *(&primeNums + 1) - primeNums << endl;
+
+	// &primeNums + 1 >> means next memory address after the full array. and primeNums decays to pointer of the first array element.
+	// if we subtract (&primeNums + 1) - primeNums by hex value, we will get the total byte of the array primeNums ( 4 byte x 7 = 28 byte )
+	// then we dereference *(&primeNums + 1) to do pointer to pointer calculation, it also convert that to int.
 		
 
 	system("pause>0");
