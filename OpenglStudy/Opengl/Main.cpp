4fc5f8b866b3b59fbcf32115839d2f0bfd379357
mainuldip <mainuldip@gmail.com>
2022-04-11 03:20:46 -0400

    docs graphics pipeline

--- OpenglStudy/Opengl/Main.cpp
+++ OpenglStudy/Opengl/Main.cpp
@@ -5,14 +5,16 @@
 int main() {
 	//std::cout << "WebGL :)" << std::endl;
 
+	// initialize glfw
 	glfwInit();
 
-
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	// if glfw version is 4.3 then major = 4 and minor = 3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // we are using glfw-3.3.7
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(700, 700, "OpenglTesting", NULL, NULL);
+	
 
 	if (window == NULL) {
 		std::cout << "Fails to create GLFW window" << std::endl;
@@ -23,6 +25,25 @@ int main() {
 
 	glfwMakeContextCurrent(window);
 
+	// initialize/load glad
+	gladLoadGL();
+	// specify the viewport of the opengl in the window
+	// in our case, the viewport goes from x=0, y=0, to x=700, y=700
+	glViewport(0,0,700,700); // maybe optional, as its working without this line
+
+	// setting color
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// execute the buffer color
+	glClear(GL_COLOR_BUFFER_BIT);
+	// swap the front buffer to back buffer. As the current/front buffer is on default color and we just create the back buffer with custom background color
+	glfwSwapBuffers(window);
+
+
+	// while loop will stop the window from being closed automatically by calling !glfwWindowShouldClose(window) event and 
+	while (!glfwWindowShouldClose(window)) {
+		// make window responsive like closeable, resizeable, dragable ect.
+		glfwPollEvents();
+	}
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
---
+++ OpenglStudy/README.md
@@ -0,0 +1,12 @@
+## OpenGL, GLFW and Glad Overview:
+
+
+### Computer Graphics Concepts:
+
+Frame: Frames changes from top left to bottom right of the screen. While the screen is printing the current frame on the screen, another frame is creating on the background. When the screen finished drawing the current frame it switcs to the next frame. And so on.
+
+Front Buffer: Current screen is fron buffer
+
+Back Buffer: Frame processing on the background or waiting for replacement of the current/front buffer is the back buffer. Voila
+
+### Graphics Pipeline:
