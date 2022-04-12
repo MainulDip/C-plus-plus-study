273831fc6d14425544d1a56f4fd54f42ff6c2bcb
mainuldip <mainuldip@gmail.com>
2022-04-12 04:37:05 -0400

    docs vertex fragment buffers

--- OpenglStudy/Opengl/Main.cpp
+++ OpenglStudy/Opengl/Main.cpp
@@ -2,6 +2,22 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
+
 int main() {
 	//std::cout << "WebGL :)" << std::endl;
 
@@ -13,8 +29,15 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// specify core profile for modern functions
 	GLFWwindow* window = glfwCreateWindow(700, 700, "OpenglTesting", NULL, NULL);
 	
+	// cordinates should bitween -1 to 1
+	GLfloat vertices[] = {
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+	};
 
 	if (window == NULL) {
 		std::cout << "Fails to create GLFW window" << std::endl;
@@ -30,6 +53,27 @@ int main() {
 	// specify the viewport of the opengl in the window
 	// in our case, the viewport goes from x=0, y=0, to x=700, y=700
 	glViewport(0,0,700,700); // maybe optional, as its working without this line
+
+
+	// open gl version of unsinged interger
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+
+	// shaders are already in the program, so delete it to release mamory
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	// setting color
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
--- OpenglStudy/README.md
+++ OpenglStudy/README.md
@@ -9,4 +9,15 @@ Front Buffer: Current screen is fron buffer
 
 Back Buffer: Frame processing on the background or waiting for replacement of the current/front buffer is the back buffer. Voila
 
-### Graphics Pipeline:
+### Graphics Pipeline: 
+It's a series of functions starts with data (vertexData[]) input and ends with creating a frame. Vertex Data[] is an array of vertices. it also contain other data like color, texture corditates. There are generally 6 steeps in Graphics pipeline =>
+1. Vertex Shader: transform vertecies position
+2. Shape Assembly: assembly/connect vertecies according to primitive type/shape (line, triangle, point etc.). A primitive is a shape. each primitive interpreate data differently. like triangle takes 3 point and draw line and make triangle, where as line takes 2 points and draw the line.
+3. Geometry Shader: create new primitive from already existing primitives by adding verticies to the base primitives.
+4. Rasterization: convert mathmatical shape into pixels. The pixels don't have color at this point.
+5. Fragment Shader: add colors to the pixel depending lighting, texture, shadows etc.
+6. Tests And Blending: blend multiple overlaping shape by creating transparancy of the pixels.
+
+### Sending data from cpu to gpu:
+it is slow, so do it on big batchs with buffers object (vertex/fragment bugger object). glGenBuffers();
+Binding
